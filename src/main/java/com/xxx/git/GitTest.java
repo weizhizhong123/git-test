ea083893dc407d9233bc1e581056db83d90c280c
zhizhong <zhizhong@qq.com>
2023-11-10 20:52:13 +0800

    first commit

---
+++ src/main/java/com/xxx/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.xxx.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("1111111");
+        System.out.println("2222222");
+        System.out.println("git push");
+    }
+}
