6ddaf79574d0cdd7bfa451d8223107acab6156fe
zhizhong <zhizhong@qq.com>
2023-11-11 10:50:49 +0800

    gitee commit

--- src/main/java/com/xxx/git/GitTest.java
+++ src/main/java/com/xxx/git/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("git push");
         System.out.println("git push2");
         System.out.println("git pull1");
-
+        System.out.println("gitee test");
     }
 }
